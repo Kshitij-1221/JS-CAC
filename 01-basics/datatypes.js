bf2eed751dd0967f93bdcfc753347e4660649217
Kshitij Gupta <75679617+Kshitij-1221@users.noreply.github.com>
2024-02-16 08:39:45 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+
+//=======PRIMITIVE-DATATYPES==========
+
+//1. Number => 2 to power 53
+//2. bigint
+//3. string => ""
+//4. boolean => true/false
+//5. null => standalone value (typeof null is an object)
+//6. undefine => when value is not define at the moment
+//7. symbol => use in react and figma
+
+//=======Reference-Datatype===========
+
+//1.Object
---
+++ 01-basics/variable.js
@@ -0,0 +1,6 @@
+const accountId = 2
+let accountEmail = "kshitij@gmail.com"
+var accountPassword = "gupta"
+
+
+console.table([accountId, accountEmail,accountPassword])
